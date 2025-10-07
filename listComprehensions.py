5f5db0ca022e36402faed789e75533a5025bb03f
Fırat Asi <firatasi@example.com>
2025-10-07 13:47:41 +0300

    s

--- listComprehensions.py
+++ listComprehensions.py
@@ -40,4 +40,53 @@ notlar = [55, 70, 85, 100, 90]
 harf_notlari = ["A" if not_ >= 90 else "B" if not_ >= 80 else "C" if not_ >= 55 else "D" for not_ in notlar]
 print(harf_notlari)
 
+names = ["Ali", "Veli", "Ayşe", "Fatma","Firat"]
+greetings = ["A" + name[1:] if name[0]=="A" else "B" + name[1:] for name in names]
+print(greetings)
+
+#list comprehensions with functions
+
+def digitSum(n):
+    total = 0
+    for digit in str(n):
+        total += int(digit)
+    return total
+
+list1 = [123, 456, 789]
+list2 = [digitSum(num) for num in list1 if num%2 != 0]
+print(list2)
+
+matris = [  [j for j in range(3)] for i in range(5)]
+print(matris)
+
+isim = "FIRAT"
+listem = [chr for chr in isim] #her karakteri listeye atar  
+print(listem)
+
+
+#lambda functions with list comprehensions
+x = lambda a: a * 2 #tek parametreli a yerine a*2 döner a yerine 5 geliyorsa 10 döner
+print(x(5))
+
+y = lambda a,b: a+b #iki parametreli a ve b toplanır
+print(y(3,5))
+
+z = lambda a,b,c: a*b*c #üç parametreli a,b,c çarpılır
+print(z(2,3,4)) 
+
+def math(n):
+    return lambda a: a * n #n parametresi dışarıdan alınır a parametresi fonksiyon çağrılırken alınır
+doubler = math(2) #n=2 olur
+print(doubler(11)) #a=11 olur 2*11=22 döner
+
+tripler = math(3) #n=3 olur
+print(tripler(11)) #a=11 olur 3*11=33 döner
+
+even_numbers = [2,4,6,8,10]
+squared_numbers = [(lambda x: x**2) (i) for i in even_numbers] #her elemanı karesini alır
+print(squared_numbers) 
+
+words = ["hello", "world", "python","ha"]
+word_length = [word + "kelimesi dırejdır" if len(word) > 3 else word + "kelimesi kındır" for word in words]
+print(word_length)
 
